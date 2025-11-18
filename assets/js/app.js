f334526dff6c23b8a9127c2f7c22ac980e307315
vekashe <98588158+vekashe@users.noreply.github.com>
2025-11-18 12:01:53 +0530

    animaiton update

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,22 @@ function hamburgerToggle() {
     var headerSec = document.querySelector('.header');
     headerSec.classList.toggle('open');
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const animatedElements = document.querySelectorAll('.animate-on-scroll');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animated');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.4
+    });
+
+    animatedElements.forEach(element => {
+        observer.observe(element);
+    });
+});
